b04d3b80743ea39a3f336aae88b87f5bce1855a2
Humairaa Saleh <humairaasaleh@gmail.com>
2021-12-13 16:34:27 +0000

    HOB-24 #comment created service db class

---
+++ HobbyWebAppProject/src/main/java/com/qa/workouts/domain/Workout.java
@@ -0,0 +1,117 @@
+package com.qa.workouts.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Workout {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String workoutName;
+
+	private String bodypart;
+	private String targetMuscle;
+	private int weight;
+	private int reps;
+	private int sets;
+
+//	CONSTRUCTORS
+
+	public Workout() {
+		super();
+	}
+
+	public Workout(String workoutName, String bodypart, String targetMuscle, int weight, int reps, int sets) {
+		super();
+		this.workoutName = workoutName;
+		this.bodypart = bodypart;
+		this.targetMuscle = targetMuscle;
+		this.weight = weight;
+		this.reps = reps;
+		this.sets = sets;
+	}
+
+	public Workout(Integer id, String workoutName, String bodypart, String targetMuscle, int weight, int reps, int sets) {
+		super();
+		this.id = id;
+		this.workoutName = workoutName;
+		this.bodypart = bodypart;
+		this.targetMuscle = targetMuscle;
+		this.weight = weight;
+		this.reps = reps;
+		this.sets = sets;
+	}
+
+//	GETTERS AND SETTERS
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getWorkoutName() {
+		return workoutName;
+	}
+
+	public void setWorkoutName(String workoutName) {
+		this.workoutName = workoutName;
+	}
+
+	public String getBodypart() {
+		return bodypart;
+	}
+
+	public void setBodypart(String bodypart) {
+		this.bodypart = bodypart;
+	}
+
+	public String getTargetMuscle() {
+		return targetMuscle;
+	}
+
+	public void setTargetMuscle(String targetMuscle) {
+		this.targetMuscle = targetMuscle;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getReps() {
+		return reps;
+	}
+
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
+
+	public int getSets() {
+		return sets;
+	}
+
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+
+//	TO STRING
+
+	@Override
+	public String toString() {
+		return "Workout [id=" + id + ", workout=" + workoutName + ", bodypart=" + bodypart + ", targetMuscle="
+				+ targetMuscle + ", weight=" + weight + ", reps=" + reps + ", sets=" + sets + "]";
+	}
+
+}
