97b1bf7e6b51c8929b3c598d7d5171a0947b39d2
Humairaa Saleh <humairaasaleh@gmail.com>
2021-12-14 10:17:54 +0000

    HOB-29 #comment get by id test created

--- HobbyWebAppProject/src/test/java/com/qa/workouts/web/WorkoutControllerIntegrationTest.java
+++ HobbyWebAppProject/src/test/java/com/qa/workouts/web/WorkoutControllerIntegrationTest.java
@@ -47,7 +47,6 @@ public class WorkoutControllerIntegrationTest {
 		String testCreatedWorkoutAsJson = this.mapper.writeValueAsString(testCreatedWorkout);
 		ResultMatcher checkStatus = status().isCreated();
 		ResultMatcher checkBody = content().json(testCreatedWorkoutAsJson);
-
 		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
 	}
 
@@ -60,7 +59,18 @@ public class WorkoutControllerIntegrationTest {
 		RequestBuilder req = get("/getAll");
 		ResultMatcher checkStatus = status().isOk();
 		ResultMatcher checkBody = content().json(json);
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+	}
 
+//	GET BY ID TEST
+	@Test
+	void testGetById() throws Exception {
+		Workout testWorkout = new Workout(1, "Leg Press", "Legs", "quads", 100, 10, 5);
+		String json = this.mapper.writeValueAsString(testWorkout);
+
+		RequestBuilder req = get("/get/1");
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(json);
 		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
 	}
 }
